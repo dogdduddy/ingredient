48b10ac5effb7bc3ffba51e4b78af0b921049b4c
jeonbyeongseon <dogdduddy@gmail.com>
2022-02-02 15:47:53 +0900

    search fragment merge

--- app/src/main/java/com/example/ingredient/MainActivity.kt
+++ app/src/main/java/com/example/ingredient/MainActivity.kt
@@ -3,14 +3,12 @@ package com.example.ingredient
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.Layout
 import android.util.Log
 import androidx.fragment.app.Fragment
 import com.example.ingredient.fragment.ExpirationDate
 import com.example.ingredient.fragment.FoodBook
 import com.example.ingredient.fragment.Search
 import com.example.ingredient.fragment.Tips
-import android.view.LayoutInflater
 import android.view.inputmethod.EditorInfo
 import androidx.recyclerview.widget.DefaultItemAnimator
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -48,52 +46,6 @@ class MainActivity : AppCompatActivity() {
                 R.id.food_book -> replaceFragment(foodbookFragment)
             }
 
-        }
-        /*
-        menu_bottom.setOnItemSelectedListener() { id ->
-            when (id) {
-                R.id.search -> replaceFragment(searchFragment)
-                R.id.expiration_date -> replaceFragment(expirationdateFragment)
-                R.id.tips -> replaceFragment(tipsFragment)
-                R.id.food_book -> replaceFragment(foodbookFragment)
-            }
-        }
-
-         */
-
-        /* 파이어베이스 입출력 예
-        // Add Data Struct
-        val user = hashMapOf(
-            "first" to "Ada",
-            "last" to "Lovelace",
-            "born" to 2022
-        )
-        // coleection name users add Data
-        db.collection("users")
-            .add(user)
-            .addOnSuccessListener { documentReference ->
-                Log.d(TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
-            }
-            .addOnFailureListener { e ->
-                Log.w(TAG, "Error adding document", e)
-            }
-
-        // Read Data
-        db.collection("users")
-            .get()
-            .addOnSuccessListener { result ->
-                for (document in result) {
-                    Log.d(TAG, "${document.id} => ${document.data}")
-                }
-            }
-            .addOnFailureListener { exception ->
-                Log.w(TAG, "Error getting documents.", exception)
-            }
-        */
-    
-        binding.testBtn.setOnClickListener {
-            val intent = Intent(this,TestActivity::class.java)
-            startActivity(intent)
         }
         // 엔터키로 검색 실행 기능
         binding.findwindow.setOnEditorActionListener { v, actionId, event ->
