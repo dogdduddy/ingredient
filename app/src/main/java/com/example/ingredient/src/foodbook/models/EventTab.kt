83a42bb9b796745a10cf08577c751ede90c46158
dogdduddy <dogdduddy@gmail.com>
2023-01-11 19:00:13 +0900

    feat(Search) : SearchMainFragment 중첩 RecyclerView로 재구현

--- app/src/main/java/com/example/ingredient/src/foodbook/models/EventTab.kt
+++ app/src/main/java/com/example/ingredient/src/foodbook/models/EventTab.kt
@@ -4,7 +4,7 @@ data class EventTab(
 	var title:String = "",
 	var effectcolor:String = "#000000",
 	var effectrange:String = "",
-	var effectsrtyle :String = "",
+	var effectstyle :String = "",
 	var eventidx :String = "",
 	var recipelist: MutableList<Recipe> = mutableListOf()
 )
--- app/src/main/java/com/example/ingredient/src/search/SearchMainFragment.kt
+++ app/src/main/java/com/example/ingredient/src/search/SearchMainFragment.kt
@@ -33,20 +33,11 @@ import kotlinx.coroutines.tasks.await
 class SearchMainFragment : Fragment() {
     private var _binding : FragmentMainBinding? = null
     private val binding get()  = _binding!!
-    private var adapter1 = SearchMainAdapter()
-    private var adapter2 = SearchMainAdapter()
-    private var adapter3 = SearchMainAdapter()
-    private lateinit var temp1:MutableList<Recipe>
-    private lateinit var temp2:MutableList<Recipe>
-    private lateinit var temp3:MutableList<Recipe>
+    private var eventTabList: MutableList<EventTab> = mutableListOf()
+    private var adapter:SearchMainAdapter = SearchMainAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        CoroutineScope(Dispatchers.Main).launch {
-            async {  persnalRecommendInit() }.await()
-            async {  recommendInit() }.await()
-            submitList()
-        }
     }
 
     override fun onCreateView(
@@ -55,33 +46,32 @@ class SearchMainFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         _binding = FragmentMainBinding.inflate(layoutInflater, container, false)
-        var recyclerView1 = binding.mainRecommendRecyclerview1
-        var recyclerView2 = binding.mainRecommendRecyclerview2
-        var recyclerView3 = binding.mainRecommendRecyclerview3
 
-        recyclerView1.layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
-        recyclerView2.layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
-        recyclerView3.layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
-
-        recyclerView1.itemAnimator = DefaultItemAnimator()
-        recyclerView2.itemAnimator = DefaultItemAnimator()
-        recyclerView3.itemAnimator = DefaultItemAnimator()
-
-        recyclerView1.adapter = adapter1
-        recyclerView2.adapter = adapter2
-        recyclerView3.adapter = adapter3
+        adapter.setItemClickListener(object : SearchMainAdapter.OnItemClickListener{
+            override fun onClick(data: Recipe) {
+                val intent = Intent(context, RecipeDialogActivity::class.java)
+                intent.putExtra("name", data.name)
+                (activity as MainActivity).addRecentRecipe(data.name, data.icon)
+                startActivity(intent)
+            }
+        })
+        binding.mainRecommendRecyclerview.adapter = adapter
+        binding.mainRecommendRecyclerview.layoutManager = LinearLayoutManager(context)
 
        return binding.root
     }
 
     fun submitList() {
-        adapter1.submitList(temp1!!)
-        adapter2.submitList(temp2)
-        adapter3.submitList(temp3)
+        eventTabList.forEachIndexed { index, eventTab ->
+
+            Log.d("subTest", "test $index : ${eventTab.effectcolor}")
+        }
+        adapter.submitList(eventTabList)
     }
 
     override fun onStart() {
         super.onStart()
+        eventTabList.clear()
         CoroutineScope(Dispatchers.Main).launch {
             async {  persnalRecommendInit() }.await()
             async {  recommendInit() }.await()
@@ -94,30 +84,6 @@ class SearchMainFragment : Fragment() {
             transaction?.add(R.id.fragment_container, searchFragment)
             transaction?.commit()
         }
-        adapter1.setItemClickListener(object: SearchMainAdapter.OnItemClickListener {
-            override fun onClick(view: View, position: Int) {
-                val intent = Intent(context, RecipeDialogActivity::class.java)
-                intent.putExtra("name", temp1!![position].name)
-                (activity as MainActivity).addRecentRecipe(temp1!![position].name, temp1!![position].icon)
-                startActivity(intent)
-            }
-        })
-        adapter2.setItemClickListener(object: SearchMainAdapter.OnItemClickListener {
-            override fun onClick(view: View, position: Int) {
-                val intent = Intent(context, RecipeDialogActivity::class.java)
-                intent.putExtra("name", temp2[position].name)
-                (activity as MainActivity).addRecentRecipe(temp2[position].name, temp2[position].icon)
-                startActivity(intent)
-            }
-        })
-        adapter3.setItemClickListener(object: SearchMainAdapter.OnItemClickListener {
-            override fun onClick(view: View, position: Int) {
-                val intent = Intent(context, RecipeDialogActivity::class.java)
-                intent.putExtra("name", temp3[position].name)
-                (activity as MainActivity).addRecentRecipe(temp3[position].name, temp3[position].icon)
-                startActivity(intent)
-            }
-        })
 
     }
     suspend fun persnalRecommendInit() {
@@ -139,34 +105,21 @@ class SearchMainFragment : Fragment() {
         }
 
         var sortedResult = result.sortedBy { it[1] }.reversed()
-        temp1 = mutableListOf(responseR[sortedResult[0][0]], responseR[sortedResult[1][0]], responseR[sortedResult[2][0]])
+        var temp1 = mutableListOf(responseR[sortedResult[0][0]], responseR[sortedResult[1][0]], responseR[sortedResult[2][0]])
 
-        binding.mainRecommendTitle1.text = "재료로 보는 추천 레시피"
-        var span1: Spannable = binding.mainRecommendTitle1.text as Spannable
-        span1.setSpan(ForegroundColorSpan(requireActivity().getColor(R.color.orange_300)), 7, 9, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
-        span1.setSpan(StyleSpan(Typeface.BOLD), 7, 9, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+        eventTabList.add(EventTab(
+            title="재료로 보는 추천 레시피",
+            effectcolor = "#" + Integer.toHexString(R.color.orange_300),
+            effectrange = "7,9",
+            effectstyle = "bold",
+            eventidx = "0",
+            recipelist = temp1))
 
     }
     suspend fun recommendInit() {
         val response = FirebaseFirestore.getInstance().collection("Event").get()
             .await().toObjects<EventTab>().sortedBy { it.eventidx }
-
-        var temp2Response = response[0]
-        var temp3Response = response[1]
-
-        temp2 = temp2Response.recipelist
-        temp3 = temp3Response.recipelist
-
-        binding.mainRecommendTitle2.text = temp2Response.title
-        binding.mainRecommendTitle3.text = temp3Response.title
-
-        var span2: Spannable = binding.mainRecommendTitle2.text as Spannable
-        var span3: Spannable = binding.mainRecommendTitle3.text as Spannable
-
-        span2.setSpan(ForegroundColorSpan(Color.parseColor(temp2Response.effectcolor)), temp2Response.effectrange.split(",")[0].toInt(), temp2Response.effectrange.split(",")[1].toInt(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
-        span2.setSpan(StyleSpan(Typeface.BOLD), temp2Response.effectrange.split(",")[0].toInt(), temp2Response.effectrange.split(",")[1].toInt(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
-        span3.setSpan(ForegroundColorSpan(Color.parseColor(temp3Response.effectcolor)), temp3Response.effectrange.split(",")[0].toInt(), temp3Response.effectrange.split(",")[1].toInt(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
-        span3.setSpan(StyleSpan(Typeface.BOLD), temp3Response.effectrange.split(",")[0].toInt(), temp3Response.effectrange.split(",")[1].toInt(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+        eventTabList.addAll(response)
     }
 
     companion object {
